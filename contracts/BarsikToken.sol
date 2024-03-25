5151fbfdbd555919ab62fb634524d77152e802b8
Tr0ublesh00ter <filip.kulas0101@gmail.com>
2024-03-25 23:20:36 +0100

    Update:Contract mainnet

--- contracts/BarsikToken.sol
+++ contracts/BarsikToken.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BarsikToken is ERC20 {
+contract BarsikToken is ERC20, Ownable {
     event TokenBurn(address indexed from, uint amount);
-    constructor() ERC20("Maxi Barsik", "MaxiB") {
+    constructor() ERC20("Maxi Barsik", "MaxiB") Ownable(msg.sender) {
         _mint(msg.sender, 1000000000 * 1e18);
     }
 
